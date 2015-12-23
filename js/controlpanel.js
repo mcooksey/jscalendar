3c5aee13bf2ef875c9a35af67e732f956a296b94
Matt Cooksey <mcooksey@csa1.com>
2015-12-23 15:51:33 -0600

    added selections, disabled, and marked dates

--- js/controlpanel.js
+++ js/controlpanel.js
@@ -11,12 +11,16 @@ var ControlPanel = (function () {
         //$('#' + controlPanelDivName).html('<div id="titleDiv" class="titleDiv">Control Panel</div>');
         self.drawControlPanel = function () {
             $('#' + self.controlPanelDivName).html('<div id="titleDiv" class="titleDiv">Control Panel</div>'
-                + '<p class="controlPanelHeader">SELECTED DATE</p>'
-                + '<p class="controlPanelText" id="selectedDate">' + self.calendarControl.selectedDate.toString("MMMM dd, yyyy") + '</p>'
-                + '<p class="controlPanelHeader">DATE PROPERTIES</p>'
-                + '<ul class="checkboxList">'
-                + '<li><input type="checkbox" id="flaggedCheckbox"/>Flagged</li>'
-                + '<li><input type="checkbox" id="disabledCheckbox"/>Disabled</li>'
+                + '<p class="controlPanelHeader">LAST SELECTED DATE</p>'
+                + '<p class="controlPanelText" id="' + self.controlPanelDivName + '_selectedDate">' + self.calendarControl.selectedDate.toString("MMMM dd, yyyy") + '</p>'
+                + '<p class="controlPanelHeader">SELECTED DATE PROPERTIES</p>'
+                + '<ul class="propertyList">'
+                + '<li><button id="' + self.controlPanelDivName + '_markedOn" class="button propertyButton">Mark</button></li>'
+                + '<li><button id="' + self.controlPanelDivName + '_markedOff" class="button propertyButton">Unmark</button></li>'
+                + '<li><button id="' + self.controlPanelDivName + '_unmarkAll" class="button propertyButton">Unmark All</button></li>'
+                + '<li><button id="' + self.controlPanelDivName + '_enabled" class="button propertyButton">Enabled</button></li>'
+                + '<li><button id="' + self.controlPanelDivName + '_disabled" class="button propertyButton">Disabled</button></li>'
+                + '<li><button id="' + self.controlPanelDivName + '_enableAll" class="button propertyButton">Enable All</button></li>'
                 + '</ul>'
                 +'<p class="controlPanelHeader">CALENDAR NAVIGATION</p>'
                 + '<div>'
@@ -26,8 +30,26 @@ var ControlPanel = (function () {
                 + '</div>'
                 + '<div class="controlPanelDiv">'
                 + '<input type="text" id="' + self.controlPanelDivName + '_dateField" placeholder="Enter a date (MM/dd/yyyy)" class="controlPanelText" />'
-                + '<button id="' + self.controlPanelDivName + '_goToDateButton" class="button controlPanelButton nextButton">Go to Date</button>'
+                + '<button id="' + self.controlPanelDivName + '_goToDateButton" class="button controlPanelButton nextButton goToDateButton">Go to Date</button>'
                 + '</div>');
+            $('#' + self.controlPanelDivName + '_markedOn').click(function () {
+                self.calendarControl.markSelected();
+            });
+            $('#' + self.controlPanelDivName + '_disabled').click(function () {
+                self.calendarControl.disableSelected();
+            });
+            $('#' + self.controlPanelDivName + '_markedOff').click(function () {
+                self.calendarControl.unmarkSelected();
+            });
+            $('#' + self.controlPanelDivName + '_enabled').click(function () {
+                self.calendarControl.enableSelected();
+            });
+            $('#' + self.controlPanelDivName + '_unmarkAll').click(function () {
+                self.calendarControl.unmarkAll();
+            });
+            $('#' + self.controlPanelDivName + '_enableAll').click(function () {
+                self.calendarControl.enableAll();
+            });
             $('#' + self.controlPanelDivName + '_back').click(function () {
                 self.calendarControl.backMonth();
             });
@@ -42,6 +64,9 @@ var ControlPanel = (function () {
                 var dateToDisplay = new XDate($('#' + self.controlPanelDivName + '_dateField').val());
                 self.calendarControl.jumpToDate(dateToDisplay);
             });
+        }
+        self.updateLastSelected = function (date) {
+            $('#' + self.controlPanelDivName + '_selectedDate').text(date.toString("MMMM dd, yyyy"));
         }
         self.drawControlPanel();
     }
